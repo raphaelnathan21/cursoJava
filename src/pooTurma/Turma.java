cbb7ed70231fd03634f3032b2065386a28f6737f
NATHAN MARTINS RAPHAEL <nathan.mraphael@senacsp.edu.br>
2024-01-23 12:02:07 -0300

    Construtores e atributos

---
+++ src/pooTurma/Turma.java
@@ -0,0 +1,35 @@
+package pooTurma;
+
+public class Turma {
+	// Atributos da Classe
+
+	String nome;
+	String telefone;
+	String email;
+
+	// Construtor
+
+	public Turma() {
+		System.out.println("_____________________");
+
+	}
+
+	// Métodos
+
+	void ligarPc() {
+		System.out.println("Computador ligado");
+	}
+
+	void logar() {
+
+		System.out.println("Login feito com sucesso");
+
+	}
+
+	void desligarPc() {
+		System.out.println("Computador desligado");
+	}
+	
+	
+
+}
